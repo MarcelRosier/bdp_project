d863d4f6f889189930cca6d3633392f4b0556def
Marcel Rosier <marcel.rosier@gmx.de>
2023-04-12 18:07:20 +0200

    - added metrics vis

--- mental_health/analysis.py
+++ mental_health/analysis.py
@@ -1,6 +1,6 @@
 import os
 
-from typing import Tuple
+from typing import List, Tuple
 
 import matplotlib.pylab as plt
 import pandas as pd
@@ -17,6 +17,8 @@ from sklearn.neural_network import MLPRegressor
 from sklearn.tree import DecisionTreeRegressor
 from xgboost.sklearn import XGBRegressor
 
+import math
+
 
 class ModelAnalysis:
 
@@ -31,6 +33,21 @@ class ModelAnalysis:
         'RandomForestRegressor': RandomForestRegressor(),
         'KNeighborsRegressor': KNeighborsRegressor()
     }
+
+    CMAP = sns.color_palette(n_colors=7).as_hex()
+    Y_COLOR = CMAP[-1]
+    PALETTE = {
+        'LinearRegression': CMAP[0],
+        'DecisionTreeRegressor': CMAP[1],
+        'MLPRegressor': CMAP[2],
+        'XGBRegressor': CMAP[3],
+        'RandomForestRegressor': CMAP[4],
+        'KNeighborsRegressor': CMAP[5],
+        'Y': Y_COLOR,
+
+    }
+    ALL_METRICS = [mean_squared_error, utils.root_mean_squared_error,
+                   r2_score, mean_absolute_error]
 
     def __init__(self, models: dict) -> None:
         self.models: dict = models
@@ -58,47 +75,55 @@ class ModelAnalysis:
         predictions = {}
         for model_name, model in self.models.items():
             predictions[model_name] = model.predict(self.X_test_std)
-        self.predicitons_test = predictions
+        self.predictions_test = predictions
         return predictions
 
-    def evaluate(self, metric=mean_absolute_error) -> dict:
+    def evaluate(self, metric=mean_absolute_error, verbose: bool = False) -> dict:
         scores = {}
         for model_name in self.models.keys():
             scores[model_name] = metric(y_true=self.Y_test,
-                                        y_pred=self.predicitons_test[model_name])
-            print(f"{model_name}: {scores[model_name]}")
+                                        y_pred=self.predictions_test[model_name])
+            if verbose:
+                print(f"{model_name}: {scores[model_name]}")
         return scores
 
-    def visualize(self, sample_range: Tuple = (0, 32)):
+    def visualize_predictions(self, sample_range: Tuple = (0, 32)) -> None:
         """
         Visualizes the predicitons of all models plotted against the ground truth Y
         by default only a subset of predicitons is plotted (0,32), this range can be adapted with @sample_range
         """
-        sample_range_start, sample_range_end = sample_range
-        preds_subset = {k: preds[sample_range_start:sample_range_end]
-                        for k, preds in self.predicitons_test.items()}
-        preds_subset['Y'] = self.Y_test[sample_range_start:sample_range_end]
+        sr_start, sr_end = sample_range
+        preds_subset = {k: preds[sr_start:sr_end]
+                        for k, preds in self.predictions_test.items()}
+        preds_subset['Y'] = self.Y_test[sr_start:sr_end]
 
         pred_df = pd.DataFrame(preds_subset)
-        cmap = sns.color_palette(n_colors=7).as_hex()
 
-        Y_COLOR = cmap[-1]
-        palette = {
-            'LinearRegression': cmap[0],  # 'b',
-            'DecisionTreeRegressor': cmap[1],  # 'black',
-            'MLPRegressor': cmap[2],  # 'r',
-            'XGBRegressor': cmap[3],  # 'c',
-            'RandomForestRegressor': cmap[4],  # 'y',
-            'KNeighborsRegressor': cmap[5],  # 'k',
-            'Y': Y_COLOR,  # 'g'
-
-        }
         fig, ax = plt.subplots(figsize=(15, 7))
         ax.set_xlabel('Datapoint index')
         ax.set_ylabel('Number of suicides (Y and model predicitons)')
         sns.scatterplot(pred_df, markers=True, alpha=.6,
-                        ax=ax, palette=palette)
+                        ax=ax, palette=self.PALETTE)
         sns.lineplot(pred_df.Y, alpha=.9, ax=ax,
-                     color=Y_COLOR, linewidth=2, legend=False)
-        sns.lineplot(pred_df[[k for k in palette.keys() if k != 'Y']],
-                     alpha=.5, ax=ax, palette=palette, linewidth=.8, legend=False)
+                     color=self.Y_COLOR, linewidth=2, legend=False)
+        sns.lineplot(pred_df[[k for k in self.PALETTE.keys() if k != 'Y']],
+                     alpha=.5, ax=ax, palette=self.PALETTE, linewidth=.8, legend=False)
+
+    def visualize_metrics(self, metrics: List = ALL_METRICS) -> None:
+        num_metrcis = len(metrics)
+        ncols = 2
+        nrows = math.ceil(num_metrcis / 2)
+        fig, ax = plt.subplots(figsize=(15, 15), nrows=nrows, ncols=ncols)
+        for col in range(ncols):
+            for row in range(nrows):
+                index = row*ncols + col
+                if index >= num_metrcis:
+                    break
+                metric = metrics[index]
+                df = pd.DataFrame({
+                    'model': self.models.keys(),
+                    'score': self.evaluate(metric=metric).values(),
+                })
+                sns.barplot(df, x='model', y='score',
+                            ax=ax[row][col], palette=self.PALETTE)
+                ax[row][col].set_title(metric.__name__)
